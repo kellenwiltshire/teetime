c6fdc01e3d0a25b476bc1bc5e31fbf66d8c3283b
Kellen <webdevelopment@kellenwiltshire.com>
2022-03-29 11:18:20 -0400

    feat: added forms and initial styling

---
+++ src/components/GolferForm.vue
@@ -0,0 +1,59 @@
+<template>
+	<h1>Golfer Name</h1>
+	<div class="card">
+		<div class="flex">
+			<h3>Name</h3>
+			<input />
+			<h3>Tee Time Restriction?</h3>
+			<input />
+			<h3>Carpool</h3>
+			<input />
+			<button>Add</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.card {
+	border-radius: 4px;
+	box-shadow: 1px 1px 40px -10px #31505f30, 0px 1px 2px 0px #31505f30;
+	background: white;
+	margin-bottom: 24px;
+}
+
+.flex {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-wrap: wrap;
+}
+
+input {
+	width: 100%;
+	padding: 14px;
+	font-size: 16px;
+	height: 100%;
+	display: block;
+}
+
+button {
+	background: #4fc08d;
+	padding: 10px 22px;
+	border: none;
+	color: white;
+	border-radius: 4px;
+	margin: 8px;
+	font-size: 16px;
+	cursor: pointer;
+	box-shadow: 1px 1px 15px -2px #212c4430;
+	transition: 0.15s;
+}
+
+button:hover {
+	background: #42aa7b;
+}
+</style>
---
+++ src/components/GolferList.vue
@@ -0,0 +1,69 @@
+<template>
+	<h1>Golfers</h1>
+	<div class="card">
+		<div class="flex">
+			<!-- This to be replaced with Component -->
+			<div class="card-inner">
+				<p>GOLFER NAME</p>
+			</div>
+			<div class="card-inner">
+				<p>GOLFER NAME</p>
+			</div>
+			<div class="card-inner">
+				<p>GOLFER NAME</p>
+			</div>
+			<div class="card-inner">
+				<p>GOLFER NAME</p>
+			</div>
+		</div>
+		<button>Generate Tee Time Schedule</button>
+	</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.card {
+	border-radius: 4px;
+	box-shadow: 1px 1px 40px -10px #31505f30, 0px 1px 2px 0px #31505f30;
+	background: white;
+	margin-bottom: 24px;
+}
+
+.card-inner {
+	padding: 16px 24px;
+}
+
+.flex {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+
+input {
+	width: 100%;
+	padding: 14px;
+	font-size: 16px;
+	height: 100%;
+	display: block;
+}
+
+button {
+	background: #4fc08d;
+	padding: 10px 22px;
+	border: none;
+	color: white;
+	border-radius: 4px;
+	margin: 8px;
+	font-size: 16px;
+	cursor: pointer;
+	box-shadow: 1px 1px 15px -2px #212c4430;
+	transition: 0.15s;
+}
+
+button:hover {
+	background: #42aa7b;
+}
+</style>
