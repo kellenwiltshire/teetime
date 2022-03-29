decd2ad35a52cdbd6226160d7cda5bda6a23d263
Kellen <webdevelopment@kellenwiltshire.com>
2022-03-29 08:48:00 -0400

    chore: removing styled component packages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
-	<div></div>
+	<CourseInformation />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import CourseInformation from '@/components/CourseInformation.vue';
 
 export default defineComponent({
 	name: 'HomeView',
-	components: {},
+	components: {
+		CourseInformation,
+	},
 });
 </script>
