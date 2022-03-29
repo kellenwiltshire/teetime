42e86930e0649824ded11bd95d953cf8cb2460ac
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-03-29 16:39:40 -0400

    chore: styling update

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,3 @@
 	</nav>
 	<router-view />
 </template>
-
-<style scoped>
-html,
-body {
-	margin: 0;
-	padding: 0;
-	background: #faffff;
-	font-size: 16px;
-}
-
-* {
-	box-sizing: border-box;
-	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	color: #3d4855;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-	margin-top: 0;
-}
-</style>
