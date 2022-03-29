ee54cbb2eb33c9e4f70b4ec8c4725e6c26712a48
Kellen <webdevelopment@kellenwiltshire.com>
2022-03-29 08:48:16 -0400

    feat: initial form created

---
+++ src/components/CourseInformation.vue
@@ -0,0 +1,28 @@
+<template>
+	<h1>Course Information</h1>
+	<div class="card">
+		<h3>Course Name</h3>
+		<input />
+		<h3>Date</h3>
+		<input />
+		<h3>Game</h3>
+		<input />
+		<h3>Start Time</h3>
+		<input />
+		<h3>Restriction Time</h3>
+		<input />
+	</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+card {
+	border-radius: 4px;
+	box-shadow: 1px 1px 40px -10px #31505f30, 0px 1px 2px 0px #31505f30;
+	background: white;
+	margin-bottom: 24px;
+}
+</style>
