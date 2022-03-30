b98e83b02bb78e48650b6e256c2faa28831592fc
Kellen <webdevelopment@kellenwiltshire.com>
2022-03-29 20:06:28 -0400

    feat: Added Pinia for State management

---
+++ src/store.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+
+interface Golfer {
+	name: string;
+	restriction: boolean;
+	carpool: string;
+}
+
+export const useStore = defineStore('golfers', {
+	state: () => ({
+		listOfGolfers: [] as Golfer[],
+	}),
+	getters: {
+		getGolfers(state) {
+			return state.listOfGolfers;
+		},
+	},
+	actions: {
+		addGolfer(newGolfer: Golfer) {
+			this.listOfGolfers.push(newGolfer);
+		},
+	},
+});
