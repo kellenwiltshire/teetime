262e9039c3f28b9105cf173cc5a7aad04c012884
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-03-31 14:25:07 -0400

    fix: Course Form styling

--- src/components/CourseInformation.vue
+++ src/components/CourseInformation.vue
@@ -1,19 +1,50 @@
 <template>
 	<h1>Course Information</h1>
 	<div class="card">
-		<form @submit.prevent="updateCourseInfo" class="flex">
-			<h3>Course Name</h3>
-			<input type="text" v-model="courseName" placeholder="Course Name" />
-			<h3>Date</h3>
-			<input type="date" v-model="date" />
-			<h3>Game</h3>
-			<input type="text" v-model="game" placeholder="Game" />
-			<h3>Interval</h3>
-			<input type="number" v-model="interval" placeholder="0" />
-			<h3>Start Time</h3>
-			<input type="time" v-model="startTime" placeholder="04:30 PM" />
-			<h3>Restriction Time</h3>
-			<input type="time" v-model="restrictionTime" placeholder="05:00 PM" />
+		<form @submit.prevent="updateCourseInfo" class="course-form">
+			<div>
+				<label for="courseName">Course Name</label>
+				<div class="input-holder">
+					<input id="courseName" name="date" type="text" v-model="courseName" placeholder="Course Name" />
+				</div>
+			</div>
+			<div>
+				<label for="date">Date</label>
+				<div class="input-holder">
+					<input id="date" name="date" type="date" v-model="date" />
+				</div>
+			</div>
+			<div>
+				<label for="game">Game</label>
+				<div class="input-holder">
+					<input id="game" name="game" type="text" v-model="game" placeholder="Game" />
+				</div>
+			</div>
+
+			<div>
+				<label for="interval">Interval</label>
+				<div class="input-holder">
+					<input id="interval" name="interval" type="number" v-model="interval" placeholder="0" required />
+				</div>
+				<p class="input-post">Time Between Tee Times</p>
+			</div>
+
+			<div>
+				<label for="startTime">Start Time</label>
+				<div class="input-holder">
+					<input id="startTime" name="startTime" type="time" v-model="startTime" required />
+				</div>
+				<p class="input-post">Initial Tee Time</p>
+			</div>
+
+			<div>
+				<label for="restrictionTime">2nd Start Time</label>
+				<div class="input-holder">
+					<input id="restrictionTime" name="restrictionTime" type="time" v-model="restrictionTime" />
+				</div>
+				<p class="input-post">2nd Start Time (if needed)</p>
+			</div>
+
 			<button>Next</button>
 		</form>
 	</div>
@@ -61,42 +92,39 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.card {
-	border-radius: 4px;
-	box-shadow: 1px 1px 40px -10px #31505f30, 0px 1px 2px 0px #31505f30;
-	background: white;
-	margin-bottom: 24px;
-}
-
-.flex {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	flex-wrap: wrap;
-}
-
-input {
-	width: 100%;
-	padding: 14px;
-	font-size: 16px;
-	height: 100%;
+label {
 	display: block;
 }
 
-button {
-	background: #4fc08d;
-	padding: 10px 22px;
-	border: none;
-	color: white;
-	border-radius: 4px;
-	margin: 8px;
-	font-size: 16px;
-	cursor: pointer;
-	box-shadow: 1px 1px 15px -2px #212c4430;
-	transition: 0.15s;
+.input-holder {
+	margin-top: 0.25rem;
 }
 
-button:hover {
-	background: #42aa7b;
+.input-post {
+	margin-top: 0.5rem;
+	font-size: 0.875rem;
+	line-height: 1.25rem;
+}
+
+.course-form {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	padding: 2rem;
+}
+
+input {
+	display: block;
+	border-radius: 0.375rem;
+	width: 100%;
+	border-color: rgb(209 213 219);
+	font-size: 0.875rem;
+	line-height: 1.25rem;
+
+	appearance: none;
+	background-color: #fff;
+	border-width: 1px;
+	padding: 0.5rem 0.75rem 0.5rem 0.75rem;
 }
 </style>
