9f639d270cc187c2737c7c32cb3139ca18bc4948
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-06 11:22:00 -0400

    feat: initial About page complete

---
+++ src/components/About/About.vue
@@ -0,0 +1,43 @@
+<template>
+	<h2>About</h2>
+	<div class="card buffer">
+		<div>
+			<p>
+				Do you have a large Golf group? Do you still try to randomize Foursome's
+				with names out of a hat or tiles out of a bag? Do you have to keep track
+				of who-carpools-with-who and who needs a later start time?
+			</p>
+			<p>
+				This might be for you then! Just follow the simple steps on the How To
+				page and generate a completely random and properly organized list of
+				Foursome's in seconds that is ready to print and send to your Group or
+				League!
+			</p>
+		</div>
+		<div>
+			<p>
+				This project was created as a spin-off from my Golf League Tracker Web
+				App(which can be viewed on my portfolio site below).
+			</p>
+		</div>
+		<div>
+			<p>
+				This project was also used as a way to learn VueJS and expand my
+				experience.
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'AboutPage',
+});
+</script>
+
+<style>
+.buffer {
+	padding: 20px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="expand container">
+		<AboutVue />
+	</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AboutVue from '@/components/About/About.vue';
+
+export default defineComponent({
+	name: 'AboutView',
+	components: {
+		AboutVue,
+	},
+});
+</script>
+
+<style scoped>
+.container {
+	padding: 24px 0;
+	max-width: 700px;
+	width: 100%;
+	margin: 0 auto;
+}
+.expand {
+	height: 100vh;
+}
+</style>
