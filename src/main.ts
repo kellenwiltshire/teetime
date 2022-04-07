6afad083d378fb3047962d792d9847e7536348af
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-07 13:34:49 -0400

    feat: added basic analytics

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,19 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 // @ts-expect-error-lintingerror
+import VueAnalytics from 'vue-ua';
+// @ts-expect-error-lintingerror
 import VueHtmlToPaper from '@/plugins/VueHtmlToPaper';
 
 import './assets/styles.css';
 
-createApp(App).use(router).use(createPinia()).use(VueHtmlToPaper).mount('#app');
+createApp(App)
+	.use(router)
+	.use(createPinia())
+	.use(VueHtmlToPaper)
+	.use(VueAnalytics, {
+		appName: 'Tee Time Generator',
+		appVersion: '0.1',
+		trackingId: 'G-BZDND8KYPJ',
+	})
+	.mount('#app');
